0bc0ee2b8ce0ebf20a7b96b8021e4b40a638bfc9
tawan21 <54388983+tawan21@users.noreply.github.com>
2019-10-27 20:32:44 +0530

    Update cat_string.c

--- cat_string.c
+++ cat_string.c
@@ -1,4 +1,4 @@
-\\Program to concatenate two strings using strcat() function
+//Program to concatenate two strings using strcat() function
 
 #include<stdio.h>  
 #include<string.h>  
@@ -9,9 +9,9 @@ char str1[30]="Wel",str2[30]="come";
 printf("\nFirst string is %s\n",str1);  
 printf("\nSecond string is %s\n",str2);  
   
-strcat(str1,str2);    \\String function to concatenate str2 with str1  
+strcat(str1,str2);    //String function to concatenate str2 with str1  
   
 printf("First string after appending second one:  %s\n",str1);  
 }  
 
-\\Code written by Tawanjot Singh ;')
+//Code written by Tawanjot Singh ;')
