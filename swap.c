cae341c630190f8c118b8b0367908cbb0d9fdf40
tawan21 <54388983+tawan21@users.noreply.github.com>
2019-10-27 22:18:20 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -14,14 +14,14 @@ scanf("%d",&y);
 printf("\n Before calling swap function\n");  
 printf("\n Value of x = %d, value of y = %d\n",x,y);  
   
-swap(x,y);    //Function called, with only values of x & y
+swap(x,y);    //Function call, with only values of x & y
   
 printf("\n After returning from swap function");  
 printf("\n Value of x = %d, value of y = %d\n",x,y);    //No change as addresses of x & y were not involved 
 }  
 
 void swap(int a,int b) 
-{    //Function definition within braces
+{    //Function definition
 int temp;  
 printf("\n Inside the function\n");  
 printf("\n Value of a = %d, value of b = %d before swap\n",a,b);  
