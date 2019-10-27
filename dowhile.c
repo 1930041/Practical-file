0cfe7eaad013ea646280dc592fdca8d82e530659
tawan21 <54388983+tawan21@users.noreply.github.com>
2019-10-27 20:59:03 +0530

    Update and rename dowhile.md to dowhile.c

--- dowhile.c
+++ dowhile.c
@@ -1,14 +1,14 @@
-#### Program to display the Sum of first 100 positive integers through do-while loop
+//Program to display the Sum of first 100 positive integers through do-while loop
 
 #include<stdio.h>  
 int main()              {  
   
 int n=1, sum=0;  
-**do{**  
+do{ 
 sum+=n;  
 n++;  
-**}**    
-**while(n<=100);**  
+}   
+while(n<=100);
   
 printf("Sum of first 100 +ve integers is %d.\n",sum);  
   
@@ -16,4 +16,4 @@ printf("Sum of first 100 +ve integers is %d.\n",sum);
 return 0;  
 }  
 
-*Code written by Tawanjot Singh ;')*
+//Code written by Tawanjot Singh ;')
