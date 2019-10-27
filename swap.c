124ee1d0cec69cb830b9b08ad75dc7f3d283144c
tawan21 <54388983+tawan21@users.noreply.github.com>
2019-10-27 22:17:06 +0530

    Update and rename swap.md to swap.c

--- swap.c
+++ swap.c
@@ -1,7 +1,7 @@
-#### Call by Value demonstration
+//Call by Value demonstration
 
 #include<stdio.h>  
-**void swap(int a,int b);**  
+void swap(int a,int b);    //Function declaration 
   
 void main()  
 {  
@@ -14,14 +14,14 @@ scanf("%d",&y);
 printf("\n Before calling swap function\n");  
 printf("\n Value of x = %d, value of y = %d\n",x,y);  
   
-**swap(x,y);**  
+swap(x,y);    //Function called, with only values of x & y
   
 printf("\n After returning from swap function");  
-printf("\n Value of x = %d, value of y = %d\n",x,y);  
+printf("\n Value of x = %d, value of y = %d\n",x,y);    //No change as addresses of x & y were not involved 
 }  
 
-**void swap(int a,int b)**  
-**{**  
+void swap(int a,int b) 
+{    //Function definition within braces
 int temp;  
 printf("\n Inside the function\n");  
 printf("\n Value of a = %d, value of b = %d before swap\n",a,b);  
@@ -29,6 +29,6 @@ temp=a;
 a=b;  
 b=temp;  
 printf("\n Value of a = %d, value of b = %d after swap\n",a,b);  
-**}**
+}
 
-*Code written by Tawanjot Singh ;')*
+//Code written by Tawanjot Singh ;')
