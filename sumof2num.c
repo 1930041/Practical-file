95a925d35341332d19a8d43e4a98daf458531eb9
tawan21 <54388983+tawan21@users.noreply.github.com>
2019-10-31 19:33:35 +0530

    Create sumof2num.c

---
+++ sumof2num.c
@@ -0,0 +1,17 @@
+//Program to display Sum of the two numbers entered
+
+#include<stdio.h>
+int main()
+{
+float n1,n2,sum;
+printf("Enter 1st,2nd number:  ");
+scanf("%f%f",&n1,&n2);
+
+sum=n1+n2;
+
+printf("Sum of 1st & 2nd number is = %.2f\n",sum);
+
+return 0;
+}
+
+//Code written by Tawanjot Singh ;')
