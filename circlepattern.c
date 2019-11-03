34c309a95163057f535d7bb29d65bd3110a39e69
tawan21 <54388983+tawan21@users.noreply.github.com>
2019-11-03 16:15:11 +0530

    Update circlepattern.c

--- circlepattern.c
+++ circlepattern.c
@@ -10,16 +10,16 @@ scanf("%d",&r);
 
 d=2*r+1;    //+1 for including centre
 char a[d][d];
-x=r+1,y=r+1;    //+1 for including centre
+x=r,y=r;    
 
-for(i=1;i<=d;i++)
-for(j=1;j<=d;j++)
+for(i=0;i<d;i++)
+for(j=0;j<d;j++)
 a[i][j]='.';
 
 float dist;
 
-for(i=1;i<=d;i++){
-for(j=1;j<=d;j++){
+for(i=0;i<d;i++){
+for(j=0;j<d;j++){
 
 dist=sqrt(pow(x-i,2)+pow(y-j,2));
 if(dist>=r-0.5 && dist<=r+0.5)    //dist needs to be approximately equal to r
@@ -27,8 +27,8 @@ a[i][j]='#';
 }
 }
 printf("\n\n");
-for(i=1;i<=d;i++){
-for(j=1;j<=d;j++){
+for(i=0;i<d;i++){
+for(j=0;j<d;j++){
 printf("%c ",a[i][j]);
 }
 printf("\n");
